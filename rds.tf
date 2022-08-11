27700457728261e1106a3f8e1f91b4730f912a1b
amoppskay <amopps4kay@gmail.com>
2022-08-10 23:00:37 -0400

    new addtion to jira 3

--- rds.tf
+++ rds.tf
@@ -1,2 +1,4 @@
 //code for rds
 //make all DB private and create in private subnets
+//made new DB
+// the DB is private
--- s3.tf
+++ s3.tf
@@ -1,2 +1,3 @@
 //code for s3
 //os of the system is windows
+//bucket name is amps-1919-00
