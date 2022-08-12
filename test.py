d41e65e6492daddf50d75fd3e22139078fb78142
amoppskay <amopps4kay@gmail.com>
2022-08-12 12:04:57 -0400

    "updated"

--- test.py
+++ test.py
@@ -3,3 +3,25 @@ print(2+2000)
 print("Hello everyone")
 print("this is our 1st python class")
 print("titi")
+
+print("Hello world!")
+
+msg = "Hello world!"
+print("msg")
+
+first_name = "Titi"
+last_name = "Ogunsina"
+full_name = first_name + " " + last_name
+print(full_name)
+
+bikes = ["bajaj", "lady", "honda", "power", "suzuki"]
+first_bike = bikes[0]
+last_bike = bikes[-1]
+for bike in bikes:
+    print(bike)
+bikes =[]
+bikes.append("bajaj")
+bikes.append("suzuki")
+bikes.append("lady")
+bikes.append("honda")
+bikes.append("power")
